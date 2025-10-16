d8add72c6f317ae85b187ea09506a3cbbb29c9f4
StudentKHong <monkeygodsunwukong@gmail.com>
2025-10-16 12:56:36 +0800

    Updated Financial Report

--- Mobile/lib/Users/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mobile/Users/home_nav.dart';
-
-
-void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeNavigation()));
