0bc65739d1d9e56c6625f8f37b7b8b881012d7d5
Omnislasher03 <rjleejj03@gmail.com>
2025-11-27 13:48:18 +0800

    commit

---
+++ Server/ApiApp/Migrations/20251127042146_changes1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changes1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "owner_user_id",
+                table: "providers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "owner_user_id",
+                table: "providers",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
