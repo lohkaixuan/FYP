97b6d105bc509c4458d4fe9695e628c20bf301ed
lohkaixuan <lohkaixuan03@gmail.com>
2025-12-01 14:49:43 +0800

    reset

--- Server/ApiApp/obj/Debug/net9.0/ApiApp.AssemblyInfo.cs
+++ Server/ApiApp/obj/Debug/net9.0/ApiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474a3666ea7db904b41a37de4f7cfe7a93713a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ade0210d98ecd5e441ee9137cc622a2133628b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
