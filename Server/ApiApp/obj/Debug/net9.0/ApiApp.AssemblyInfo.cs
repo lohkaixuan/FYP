a48d03f121f3b7134dd3a8d099dbb713fa16df59
StudentKHong <monkeygodsunwukong@gmail.com>
2026-01-15 09:52:06 +0800

    .

--- Server/ApiApp/obj/Debug/net9.0/ApiApp.AssemblyInfo.cs
+++ Server/ApiApp/obj/Debug/net9.0/ApiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938f6936691b5621b67266c91bad21e35d2816d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1b372e2d09a0a786c38fe3ce5ef7d62e9496fec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
